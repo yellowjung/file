f9fb2a1088cee35faf26954327b9e4e2c389c970
Park Ju Hyung <qkrwngud825@gmail.com>
2018-02-02 00:38:53 +0900

    Install to /tmp/file
    Until we figure out how to link libmagic statically
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- iter.sh
+++ iter.sh
@@ -3,6 +3,5 @@
 # Usage : ./iter.sh /path/to/iterate > /path/to/save/log
 # Sort  : cat log | grep -v empty | sort -k3 -g
 
-FILE=./src/.libs/file
-MAGIC=./magic/magic.mgc
-find "$@" -xdev -type f | xargs -n8192 -P$(grep -c ^processor /proc/cpuinfo) -d '\n' $FILE -m $MAGIC -f -
+FILE=/tmp/file/bin/file
+find "$@" -xdev -type f | xargs -n8192 -P$(grep -c ^processor /proc/cpuinfo) -d '\n' $FILE -f -
