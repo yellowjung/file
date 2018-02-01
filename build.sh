80ceb728af0573682375928161acbbe07f252f3f
Park Ju Hyung <qkrwngud825@gmail.com>
2018-02-02 00:21:26 +0900

    build.sh: add instructions to prepare dependencies
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# sed -i -e 's@# deb-src@deb-src@g'
+# apt update
+# apt build-dep file
+
 autoreconf -f -i
 CC=clang CFLAGS='-O3 -mtune=haswell -flto' LDFLAGS='-flto -lm' ./configure --disable-zlib --prefix=/tmp/file && make -j$(grep -c ^processor /proc/cpuinfo)
