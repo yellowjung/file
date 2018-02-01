b0ad6c1b20d20d01b79e94f9b6776471392c20cc
Park Ju Hyung <qkrwngud825@gmail.com>
2018-02-02 00:17:20 +0900

    build.sh: run make after autoconf succeeds
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 autoreconf -f -i
-CC=clang CFLAGS='-O3 -mtune=haswell -flto' LDFLAGS='-flto -lm' ./configure --disable-zlib --prefix=/tmp/file
+CC=clang CFLAGS='-O3 -mtune=haswell -flto' LDFLAGS='-flto -lm' ./configure --disable-zlib --prefix=/tmp/file && make -j$(grep -c ^processor /proc/cpuinfo)
